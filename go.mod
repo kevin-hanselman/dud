092ede60c2fd7071e294de6ccc7efa78fc174e8f
Kevin Hanselman <kevin.hanselman@gmail.com>
2020-03-07 11:55:58 -0500

    unit test stage.go using testify/mock

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/c2h5oh/datasize v0.0.0-20200112174442-28bbd4740fee
 	github.com/google/go-cmp v0.4.0
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.5.1
 )
--- stage/stage_test.go
+++ stage/stage_test.go
@@ -3,8 +3,24 @@ package stage
 import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/kevlar1818/duc/artifact"
+	"github.com/kevlar1818/duc/strategy"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
+
+type mockCache struct {
+	mock.Mock
+}
+
+func (c *mockCache) Commit(workingDir string, art *artifact.Artifact, strat strategy.CheckoutStrategy) error {
+	args := c.Called(workingDir, art, strat)
+	return args.Error(0)
+}
+
+func (c *mockCache) Checkout(workingDir string, art *artifact.Artifact, strat strategy.CheckoutStrategy) error {
+	args := c.Called(workingDir, art, strat)
+	return args.Error(0)
+}
 
 func TestSetChecksum(t *testing.T) {
 	s := Stage{
@@ -42,4 +58,66 @@ func TestSetChecksum(t *testing.T) {
 	if s.Checksum == origChecksum {
 		t.Fatal("changing stage.WorkingDir should have affected checksum")
 	}
+}
+
+func TestCommit(t *testing.T) {
+	t.Run("Copy", func(t *testing.T) { testCommit(strategy.CopyStrategy, t) })
+	t.Run("Link", func(t *testing.T) { testCommit(strategy.LinkStrategy, t) })
+}
+
+func testCommit(strat strategy.CheckoutStrategy, t *testing.T) {
+	stg := Stage{
+		Checksum:   "",
+		WorkingDir: "workDir",
+		Outputs: []artifact.Artifact{
+			{
+				Checksum: "",
+				Path:     "foo.txt",
+			},
+			{
+				Checksum: "",
+				Path:     "bar.txt",
+			},
+		},
+	}
+
+	cache := mockCache{}
+	for i := range stg.Outputs {
+		cache.On("Commit", "workDir", &stg.Outputs[i], strat).Return(nil)
+	}
+
+	stg.Commit(&cache, strat)
+
+	cache.AssertExpectations(t)
+}
+
+func TestCheckout(t *testing.T) {
+	t.Run("Copy", func(t *testing.T) { testCheckout(strategy.CopyStrategy, t) })
+	t.Run("Link", func(t *testing.T) { testCheckout(strategy.LinkStrategy, t) })
+}
+
+func testCheckout(strat strategy.CheckoutStrategy, t *testing.T) {
+	stg := Stage{
+		Checksum:   "",
+		WorkingDir: "workDir",
+		Outputs: []artifact.Artifact{
+			{
+				Checksum: "",
+				Path:     "foo.txt",
+			},
+			{
+				Checksum: "",
+				Path:     "bar.txt",
+			},
+		},
+	}
+
+	cache := mockCache{}
+	for i := range stg.Outputs {
+		cache.On("Checkout", "workDir", &stg.Outputs[i], strat).Return(nil)
+	}
+
+	stg.Checkout(&cache, strat)
+
+	cache.AssertExpectations(t)
 }
