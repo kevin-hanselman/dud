c3fcb67b62cea46e2454ec40782a1130ffe9badd
Kevin Hanselman <kevin.hanselman@protonmail.com>
2020-12-02 21:21:55 -0500

    fix root cli command 404

--- docs/en.search.min.d3d05e446f22f664634349c07af5e585c1506c2a0a75e9f764e283ce286a75a9.js
+++ docs/en.search.min.d3d05e446f22f664634349c07af5e585c1506c2a0a75e9f764e283ce286a75a9.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/dud/flexsearch.min.js');loadScript('/dud/en.search-data.min.188c5e85f4c3b63672f11ed8149df345fa04e5d191ef0de6fc09c8adbb6dd73d.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/dud/flexsearch.min.js');loadScript('/dud/en.search-data.min.c169712373d33953c0dde229401162f5bc8b0b446600fdb06b5360d363e133e9.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
