65cad972a60d9a8c243d6c087e445f616003ef89
Kevin Hanselman <kevin.hanselman@protonmail.com>
2021-03-14 13:56:47 -0400

    expand profiling/tracing test

--- integration/tests/enable_profiling/run.sh
+++ integration/tests/enable_profiling/run.sh
@@ -10,7 +10,23 @@ if ! test -s dud.pprof; then
     exit 1
 fi
 
+# Commands that fail should still generate profiling output.
+dud --profile stage add foo.yaml || true
+
+if ! test -s dud.pprof; then
+    echo 'profiling output does not exist or is empty' 1>&2
+    exit 1
+fi
+
 dud --trace stage gen -o foo.txt
+
+if ! test -s dud.trace; then
+    echo 'tracing output does not exist or is empty' 1>&2
+    exit 1
+fi
+
+# Commands that fail should still generate tracing output.
+dud --trace stage add foo.yaml || true
 
 if ! test -s dud.trace; then
     echo 'tracing output does not exist or is empty' 1>&2
