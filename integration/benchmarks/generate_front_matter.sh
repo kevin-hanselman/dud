36bd501cbfcc447c7da1423f5ce208faa4113666
Kevin Hanselman <kevin.hanselman@protonmail.com>
2021-04-10 11:37:37 -0400

    misc: clean bench OS, rm stale CI commands

--- integration/benchmarks/generate_front_matter.sh
+++ integration/benchmarks/generate_front_matter.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 BENCH_CPU=$(grep -m1 '^model name' /proc/cpuinfo | cut -d':' -f2 | xargs)
 BENCH_SYS_MEM_GB=$(free --giga | grep '^Mem' | awk '{print $2}')
-BENCH_OS=$(uname -srmo)
+# Strip the minor version and misc qualifiers
+BENCH_OS=$(uname -srm | sed -e 's;\([0-9]\.[0-9]\+\)\.\S\+;\1;')
 
 cat << EOF
 # Benchmarks
