52efa393f22da178280f9ad0fc9fcaf78b861a67
Kevin Hanselman <kevin.hanselman@protonmail.com>
2022-03-05 20:12:50 +0000

    Fix project unlocking and UI bug (#92)
    * fix project unlocking
    
    * announce commit before all non-recursive inputs
    
    fixes #91

--- integration/tests/lock_project/run.sh
+++ integration/tests/lock_project/run.sh
@@ -7,24 +7,35 @@ dud stage gen -o foo.txt -- sleep 10 > sleep.yaml
 
 dud stage add sleep.yaml
 
-# This loop is mainly to ensure this concurrent test code is working well.
-for _ in $(seq 1 10); do
-    dud run &
+dud run &
+dud_run_pid=$!
 
-    # Give 'dud run' enough time to start
-    sleep 0.02
+# Give 'dud run' enough time to create the lock file.
+sleep 0.05
 
-    if dud status; then
-        echo 1>&2 'expected second concurrent dud command to fail'
-        exit 1
-    fi
+if dud status; then
+    echo 1>&2 'TEST FAIL: expected second concurrent dud command to fail'
+    exit 1
+fi
 
-    # Kill dud run's command and wait for it stop. This should cause 'run' to
-    # fail, but we still expect Dud to release the project lock file.
-    killall sleep
-    wait
-    if test -f .dud/lock; then
-        echo 1>&2 'expected first dud command to clean up its lock file'
-        exit 1
-    fi
-done
+# We CANNOT send SIGINT here, as non-interactive Bash won't send it to its job. TIL.
+# https://unix.stackexchange.com/a/677742/49491
+kill -SIGTERM "$dud_run_pid"
+wait
+
+# Having both of these tests is a bit redundant, but better safe than sorry.
+err=0
+if ! test -f .dud/lock; then
+    echo 1>&2 'TEST FAIL: expected interrupted dud command to leave its lock file'
+    err=$((err + 1))
+fi
+
+if dud status; then
+    echo 1>&2 'TEST FAIL: expected orphaned lock file to cause new dud command to fail'
+    err=$((err + 1))
+fi
+
+# Clean up orphaned 'dud run' process
+pkill sleep
+
+exit "$err"
